ff925c57e282ff727403269fcb2c9590c224b185
deantosh <deantoshdaiddoh@gmail.com>
2023-08-08 12:48:21 -0700

    task 1: file updated

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    digit = -1 *((number * -1) % 10)
+    digit = -1 * ((number * -1) % 10)
 else:
     digit = number % 10
 if digit > 5:
