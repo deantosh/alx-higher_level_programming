92d98f15ad1c36e26745df699a5dfb8c630ff20a
deantosh <deantoshdaiddoh@gmail.com>
2023-08-15 09:22:22 -0700

    Task 14: cpython #0: python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,34 @@
+/*
+ * Author: Deantosh M Daiddoh
+ * File: 100-print_python_list_info.c
+ */
+
+#include <Python.h>
+#include <listobject.h>
+#include <object.h>
+
+/*include function prototype*/
+void print_python_list_info(PyObject *p);
+
+/**
+ * print_python_list_info - Prints information about python lists.
+ * @p: The python object containing the list.
+ *
+ * Return: void.
+ */
+void print_python_list_info(PyObject *p)
+{
+	long int list_size = PyList_Size(p);
+	PyListObject *obj = (PyListObject *)p; /*cast*/
+	int i = 0;
+
+	printf("[*] Size of the python List = %li\n", list_size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+
+	/*loop though list to print items*/
+	while (i < list_size)
+	{
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+		i++;
+	}
+}
