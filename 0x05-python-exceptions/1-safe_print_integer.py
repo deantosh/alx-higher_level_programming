537e5d7747fe57ce95bd5b82e6555b927b71082c
deantosh <deantoshdaiddoh@gmail.com>
2023-08-29 01:57:12 -0700

    task 1: solution modified

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        x = isinstance(value, int)
-        if x:
-            print("{:d}".format(value), end="\n")
-            return True
-        else:
-            return False
-    except typeError:
-        print("Error: invalid value")
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        pass
+        
