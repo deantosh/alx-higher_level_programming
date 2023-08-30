a5b31d1e15bef6b8e3055bcda98c500b940f3798
deantosh <deantoshdaiddoh@gmail.com>
2023-08-30 10:12:02 -0700

    task 2: solution added

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """initializes the data"""
+    def __init__(self, size=0):
+        """raise an error"""
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be  >= 0")
+        self.__size = size
