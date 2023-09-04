0efafc394e25f6b21094e61e14643acef14e2193
deantosh <deantoshdaiddoh@gmail.com>
2023-09-04 14:22:56 -0700

    task 1: solution modified

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,22 +3,19 @@
 
 
 class Rectangle:
-    """create a rectangle"""
-
+    """represents a rectangle"""
     def __init__(self, width=0, height=0):
         """initializes the data
            Args:
                  width - width of the rectangle
                  height - height of the rectangle
         """
-
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         """gets the width"""
-
         return (self.__width)
 
     @width.setter
@@ -27,18 +24,15 @@ class Rectangle:
            Args:
                 width - width of the rectangle
         """
-
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be  >= 0")
-
         self.__width = value
 
     @property
     def height(self):
         """gets the height value"""
-
         return (self.__height)
 
     @height.setter
@@ -47,10 +41,8 @@ class Rectangle:
            Args:
                 height - height of the rectangle
         """
-
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height musst be >= 0")
-
         self.__height = value
