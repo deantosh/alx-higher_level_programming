0362d74f8d9ff33c3304a37804786d082559bbc9
deantosh <deantoshdaiddoh@gmail.com>
2023-09-05 00:56:10 -0700

    task 1: solution added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
                  width - width of the rectangle
                  height - height of the rectangle
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -27,7 +27,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be  >= 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -44,5 +44,5 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height musst be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
