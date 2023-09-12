caaea9b9493173756d75c58e4a63f3e1cf674b14
deantosh <deantoshdaiddoh@gmail.com>
2023-09-12 13:09:39 -0700

    task 0: file modifiedd

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""defines a function that reads a text file"""
+
 
 def read_file(filename=""):
-    with open(filename, "r" encoding="UTF8") as file_obj:
+    """opens text file to read content"""
+    with open(filename, "r", encoding="UTF8") as file_obj:
         read_data = file_obj.read()
         print(read_data)
