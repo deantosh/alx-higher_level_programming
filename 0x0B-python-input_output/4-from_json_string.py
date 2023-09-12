ded7b2db9b0990fab04c4d9c7a48f430ba5e7e7d
deantosh <deantoshdaiddoh@gmail.com>
2023-09-12 13:38:04 -0700

    task 4: solution added

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""module: defines a function that converts JSON string
+   into an object.
+"""
 import json
 
 
 def from_json_string(my_str):
+    """returns:
+              object
+    """
     obj = json.loads(my_str)
     return (obj)
