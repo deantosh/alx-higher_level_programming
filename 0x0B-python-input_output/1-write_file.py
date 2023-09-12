9afd7fcc058732c07118f0c44cb47b6dc4087c2a
deantosh <deantoshdaiddoh@gmail.com>
2023-09-12 13:15:45 -0700

    task 1: solution added

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""module: defines a function to write to a file"""
+
 
 def write_file(filename="", text=""):
+    """returns the bytes wrote"""
     with open(filename, "w") as file_obj:
         num_bytes = file_obj.write(text)
         return (num_bytes)
