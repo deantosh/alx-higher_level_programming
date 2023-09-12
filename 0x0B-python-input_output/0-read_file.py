146c5716463aa0ac52682d3a00ebffedeeb9fc4c
deantosh <deantoshdaiddoh@gmail.com>
2023-09-12 13:18:36 -0700

    task 0: solution added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""defines a function that reads a text file"""
+"""module: defines a function that reads a text file"""
 
 
 def read_file(filename=""):
-    """opens text file to read content"""
-    with open(filename, "r", encoding="UTF8") as file_obj:
+    """prints the read file data"""
+    with open(filename, "r") as file_obj:
         read_data = file_obj.read()
         print(read_data, end="")
