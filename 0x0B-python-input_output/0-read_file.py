d003acc4ddf43801fe83f55f0ac41c815ac63649
deantosh <deantoshdaiddoh@gmail.com>
2023-09-12 13:12:01 -0700

    task 0: solution modified

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """opens text file to read content"""
     with open(filename, "r", encoding="UTF8") as file_obj:
         read_data = file_obj.read()
-        print(read_data)
+        print(read_data, end="")
