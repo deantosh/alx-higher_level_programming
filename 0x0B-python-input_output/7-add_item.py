12a9e93167e54923f979cf3f966a91585d768fad
deantosh <deantoshdaiddoh@gmail.com>
2023-09-13 02:42:22 -0700

    task 7: solution added

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    
+    try:
+        # get data from JSON file
+        my_list = load_from_json_file("add_item.json")
+    except:
+        my_list = []
+
+    # add items to list if provided
+    my_list.extend(sys.argv[1:])
+
+    # save list to JSON file
+    save_to_json_file(my_list, "add_item.json")
