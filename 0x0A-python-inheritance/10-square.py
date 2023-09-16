f76292d396da631b1c973cad8a824576d9a4ff68
deantosh <deantoshdaiddoh@gmail.com>
2023-09-16 12:09:57 -0700

    task 10: create a square class that inherits from subclass rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module: defines class - Square
+           that inherits from a subclass rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """defines a square"""
+
+    def __init__(self, size):
+        """initializes the square"""
+
+        """validate the value of size attribute"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+        self.__size = size
