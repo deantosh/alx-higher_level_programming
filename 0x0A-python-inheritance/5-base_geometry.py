6ca0425f6c4289453a1d07645989307aac35c87b
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2023-10-24 10:18:57 +0300

    repo deleted

--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module: defines BaseGeometry class"""
-
-
-class BaseGeometry:
-    """does nothing"""
-    pass
--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module: defines BaseGeometry class"""
-
-
-class BaseGeometry:
-    """defines BaseGeometry class"""
-    def area(self):
-        """defines area"""
-        raise Exception("area() is not implemented")
