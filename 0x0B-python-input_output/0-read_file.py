33e643fbe158c08471082d297ffea30fbebb31ed
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2023-10-24 10:24:21 +0300

    repo deleted

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""module: defines a function that reads a text file"""
-
-
-def read_file(filename=""):
-    """prints the read file data"""
-    with open(filename, "r") as file_obj:
-        read_data = file_obj.read()
-        print(read_data, end="")
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""module: defines a class-to-JSON function"""
-
-
-def class_to_json(obj):
-    """returns:
-              a dictionary representation with simple data structure
-    """
-    return obj.__dict__
