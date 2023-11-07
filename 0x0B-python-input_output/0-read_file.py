91261f8a3ffe6c1ecb76e393962fc4563600ba87
deantosh <deantoshdaiddoh@gmail.com>
2023-11-07 09:13:49 -0800

    task 0: solution added

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Reads all the content of the file"""
     with open(filename, encoding="utf-8") as a_file:
-        print(a_file.read())
+        print(a_file.read(), end="")
