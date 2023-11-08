9da078fe861621a8148140f30a3e3dc6d49990df
deantosh <deantoshdaiddoh@gmail.com>
2023-11-08 08:34:38 -0800

    task 7: solution added

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""Script adds command args to a python list"""
+"""Module adds command args to a python list"""
+
+
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = \
+        __import__("6-load_from_json_file").load_from_json_file
 
-# create file
-filename = "add_item.json"
-
-n = len(sys.argv)
-
-if n > 1:
-    # get list of all command line args
+    filename = "add_item.json"
     arg_list = sys.argv[1:]
 
-try:
-    # get object from json file
-    obj = load_from_json_file(filename)
-    # add arg list to obj
+    try:
+        # get object from json file
+        obj = load_from_json_file(filename)
+
+    except FileNotFoundError:
+        obj = []
+
     obj.extend(arg_list)
-
-except FileNotFoundError:
-    obj = []
-
-# save the obj to json file
-save_to_json_file(obj, filename)
+    # save the obj to json file
+    save_to_json_file(obj, filename)
