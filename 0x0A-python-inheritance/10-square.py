71b327c9599afdc5a1558f5c73634c735fafe55f
deantosh <deantoshdaiddoh@gmail.com>
2023-11-30 01:39:09 -0800

    task 10: solution added

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module defines a class Square that inherits from Rectangle class"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square"""
+    def __init__(self, size):
+        """
+           initialize class
+        """
+        self.__size = size
+        super().__init__(self.__size, self.__size)
