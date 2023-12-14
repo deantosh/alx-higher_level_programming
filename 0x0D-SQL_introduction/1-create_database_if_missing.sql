efec8fbe7426c1cfaf518d8128208692c0050b46
deantosh <deantoshdaiddoh@gmail.com>
2023-12-14 06:35:29 -0800

    task 1: solution added

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- creates a database hbtn_0c_0 in MySQL server
+-- if database already exists, script should not fail
+-- not allowed to use the SELECT or SHOW statements
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
