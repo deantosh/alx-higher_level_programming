b2ad8974ee3f155513193cabc0e13041c147b6de
deantosh <deantoshdaiddoh@gmail.com>
2023-12-15 09:24:16 -0800

    task 12: solution added

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- updates the score of Bob to 10 in the table second_table
+-- use the name and not Bob's id
+-- database will be passed as an argument of the mysql command
+
+UPDATE second_table SET score = 10 WHERE name = "Bob";
+
