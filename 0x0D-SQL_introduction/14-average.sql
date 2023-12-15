230cab45385e2e0973b09a9325814e1aeec74723
deantosh <deantoshdaiddoh@gmail.com>
2023-12-15 12:28:42 -0800

    task 14: solution added

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- computes the score average of all records in the table second_table
+-- database name hbtn_0c_0 to be passed as an argument of the mysql command
+-- the result column name should be average
+
+SELECT AVG(score) as average FROM second_table;
