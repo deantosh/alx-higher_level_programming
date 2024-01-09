90b45c0b838bcd82bd98a41eba41969e348e4790
deantosh <deantoshdaidddoh@gmail.com>
2024-01-09 08:14:33 -0800

    Task 9: solution added

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+if (process.argv.length === 4) {
+  const num1 = parseInt(process.argv[2]);
+  const num2 = parseInt(process.argv[3]);
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log('NaN');
+  } else {
+    console.log(num1 + num2);
+  }
+} else {
+  console.log('NaN');
+}
