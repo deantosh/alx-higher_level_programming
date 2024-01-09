ac4c4be46a6ad0b99dc2b7196aaf82751a361755
deantosh <deantoshdaidddoh@gmail.com>
2024-01-09 11:04:46 -0800

    Task 11: solution added

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ if (len > 3) {
   for (let i = 2; i < len; i++) {
     numArray.push(parseInt(process.argv[i]));
   }
-  numArray.sort(function (a, b) {return a - b}); 
+  numArray.sort(function (a, b) { return a - b; });
   console.log(numArray[len - 4]);
 } else {
   console.log(0);
