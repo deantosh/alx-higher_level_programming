445dc74d33da612f4f74b819fc571ac104bc51c6
deantosh <deantoshdaidddoh@gmail.com>
2024-01-09 07:56:12 -0800

    Task 8: solution added

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+if (process.argv[2]) {
+  const num = parseInt(process.argv[2]);
+  if (isNaN(num)) {
+    console.log('Missing size');
+  } else {
+    for (let i = 0; i < num; i++) {
+      let square = '';
+      for (let j = 0; j < num; j++) {
+        square += 'X';
+      }
+      console.log(square);
+    }
+  }
+} else {
+  console.log('Missing size');
+}
