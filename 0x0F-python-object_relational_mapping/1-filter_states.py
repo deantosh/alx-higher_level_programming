fa5ff6dd61d355d6ad759689e25137998fb511ad
deantosh <deantoshdaiddoh@gmail.com>
2024-01-13 04:30:51 -0800

    Task 1: solution added

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script lists a states from the
+database hbtn_0e_0_usa.
+"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    """
+    Access to the db and get the states.
+    """
+    # Create database connection
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306
+    )
+    # cursor obect to connect to database
+    cursor = conn.cursor()
+    # Execute SQL queries
+    query = "SELECT * FROM states WHERE name REGEXP '^N'"
+    cursor.execute(query)
+    # Fetch results
+    results = cursor.fetchall()
+    # Print results
+    for row in results:
+        print(row)
