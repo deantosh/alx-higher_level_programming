71a8c299266ffc85ac6ba2320d837ffdc7ad10c8
deantosh <deantoshdaiddoh@gmail.com>
2024-01-14 11:42:37 -0800

    Task 10: solution added

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Script lists all State objects that contain the letter a
+from the database `hbtn_0e_6_usa`
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def create_db_engine(username, password, database):
+    """Creates and returns database engine"""
+    # creata connection string
+    conn_string = "mysql://{}:{}@{}:{}/{}".format(
+        username, password, 'localhost', 3306, database
+    )
+
+    # create engine
+    engine = create_engine(conn_string)
+
+    # return engine
+    return engine
+
+
+if __name__ == '__main__':
+    # Create variabes to pass to func
+    username, password, database, state_name = argv[1:]
+
+    # Create engine
+    engine = create_db_engine(username, password, database)
+
+    # Bind engine to base class
+    Base.metadata.bind = engine
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query first state object
+    states = session.query(State).filter(State.name == state_name).all()
+
+    # Print first state
+    if states:
+        for state in states:
+            print(f'{state.id}')
+    else:
+        print("Not found")
+
+    # Close the session
+    session.close()
