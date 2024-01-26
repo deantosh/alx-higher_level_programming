ebee49bba01534f90b73fba0e219ea5ba273d91a
deantosh <deantoshdaiddoh@gmail.com>
2024-01-26 10:08:19 -0800

    Task 1: solution added

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,13 +3,14 @@
 import sys
 from urllib.request import Request, urlopen
 
-# get url passed as argument
-url = sys.argv[1]
+if __name__ == '__main__':
+    # get url passed as argument
+    url = sys.argv[1]
 
-req = Request(url)
+    req = Request(url)
 
-with urlopen(req) as response:
-    # get headers
-    headers = response.headers
-    value = headers.get('X-Request-Id')
-    print(value)
+    with urlopen(req) as response:
+        # get headers
+        headers = response.headers
+        value = headers.get('X-Request-Id')
+        print(value)
