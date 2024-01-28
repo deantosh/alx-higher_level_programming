8724c0189f7b7487beab9a2ac2b0b7e298361731
deantosh <deantoshdaiddoh@gmail.com>
2024-01-28 08:23:48 -0800

    Task 8: solution added

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-: <<'COMMENT'
-Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-COMMENT
-
-response=$(curl -sX POST --data-binary "@$2" "$1")
-
-echo "$response"
+# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+curl -sX POST --data-binary "@$2" "$1")
