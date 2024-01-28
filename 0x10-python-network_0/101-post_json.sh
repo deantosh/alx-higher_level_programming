3a76b76f77a7b59f49850d7ee3695937443d0945
deantosh <deantoshdaiddoh@gmail.com>
2024-01-28 08:56:41 -0800

    Task 8: solution added

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -sXH POST "Content-Type: application/json" --data-binary "@'$2'" "$1"
+curl -s -X POST -H "Content-Type: application/json" --data-binary "@$2" "$1"
