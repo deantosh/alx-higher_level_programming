56cbb2889e049aeb3e2ea882b512dde8006c5ee5
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-03-20 01:51:31 -0700

    task 17: solution updated

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -4,11 +4,15 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use a maximum of two SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT DISTINCT tv_genres.name AS name
+SELECT DISTINCT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres
+WHERE tv_genres.id NOT IN (
+SELECT tv_genres.id
+FROM tv_genres
+INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
+INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title != "Dexter" OR tv_shows.title IS NULL
+WHERE tv_shows.title = "Dexter"
+)
 ORDER BY tv_genres.name ASC;
