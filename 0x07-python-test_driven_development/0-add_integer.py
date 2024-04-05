e23743a43aa6f8c1a280d6db28c16732064d3940
deantosh <deantoshdaiddoh@gmail.com>
2024-04-05 07:05:03 -0700

    task 0: solution added

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Module defines a function that adds two integers"""
+"""
+Module has a function `add_integer()` that adds 2 integers.
+
+Requirements:
+  - Prototype: def add_integer(a, b=98):
+  - a and b must be integers or floats, otherwise raise a TypeError
+    exception with the message a must be an integer or b must be an integer
+  - a and b must be first casted to integers if they are float
+  - Returns an integer: the addition of a and b
+  - You are not allowed to import any module
+"""
 
 
 def add_integer(a, b=98):
-    """return:
-              sum of the two numbers
-    """
+    """returns sum of two numbers"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
