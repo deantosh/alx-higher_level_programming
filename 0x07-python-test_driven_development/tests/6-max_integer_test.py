668891c930c92f330ca99020b576f722236c3d93
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-04-07 13:39:58 +0300

    task 5: test file added

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Tests for ``max_integer()`` function that takes in a list and returns maximum
+integer in that list.
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """test cases"""
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer())
+
+    def test_valid_list(self):
+        self.assertEqual(max_integer([1]), 1)
+        self.assertEqual(max_integer([5, 1, 6, 3]), 6)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([10, -5, 20, -30, 15]), 20)
