5003f5b8c7c0a577ef5de78e055f55d4871c81e9
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-04-08 19:33:12 +0300

    task 2: module documentation updated

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,6 +19,10 @@ Requirements:
                 message height must be >= 0
  3. Instantiation with optional width and height: def __init__(self, width=0,
     height=0):
+ 4. Public instance method: def area(self): that returns the rectangle area
+ 5. Public instance method: def perimeter(self): that returns the rectangle 
+    perimeter:
+       - if width or height is equal to 0, perimeter is equal to 0
 """
 
 
