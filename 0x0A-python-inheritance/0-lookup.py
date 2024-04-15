e4d14e7a6c7785748eb4d74317a5311dba375bc3
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-04-15 11:22:49 +0300

    files deleted

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module gets an object's list of attribute and methods"""
-
-
-def lookup(obj):
-    """return:
-              list of available attributes and methods of an object
-    """
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Module defines an empty class BaseGeometry"""
-
-
-class BaseGeometry:
-    """empty class"""
-    pass
