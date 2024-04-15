0d7b78a2cf5df3e3e8ac71ffb5979c118bdd8b85
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-04-15 12:18:41 +0300

    task 1: solution added

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+"""
+Module defines a class ``MyList`` that inherits from list.
+"""
+
+
+class MyList(list):
+    """defines a subclass of the list class"""
+    def print_sorted(self):
+        """prints a sorted list"""
+        print(sorted(self))
