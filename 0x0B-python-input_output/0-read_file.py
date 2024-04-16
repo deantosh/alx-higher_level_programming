d07abc0b67e4024d8559c64503e5400a91938a5c
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-04-16 15:27:28 +0300

    files deleted

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Read a text file"""
-
-
-def read_file(filename=""):
-    """Reads all the content of the file"""
-    with open(filename, encoding="utf-8") as a_file:
-        print(a_file.read(), end="")
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module gets the dictionary description of an object"""
-
-
-def class_to_json(obj):
-    """return:
-             obj dict
-    """
-    return obj.__dict__
