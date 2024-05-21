cf8a44a399b7aa1fb6e138926fc42bbec33b943c
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-05-21 23:25:56 +0300

    task 9: solution added

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,9 +6,11 @@ CREATE TABLE IF NOT EXISTS second_table(
   score INT
 );
 
-INSERT INTO second_table(id, name, score)
-VALUES
+INSERT INTO second_table(id, name, score) VALUES
     (1, "John", 10),
     (2, "Alex", 3),
     (3, "BOb", 14),
-    (4, "George", 8);
+    (4, "George", 8)
+ON DUPLICATE KEY UPDATE
+    name = VALUES(name),
+    score = VALUES(score);
