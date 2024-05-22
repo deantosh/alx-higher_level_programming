aed3f71a5e5888e14826908703740d678961e241
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-05-22 15:11:23 +0300

    task 8: solution added

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -8,4 +8,4 @@
 -- find the id in states table
 SET @california_id = (SELECT id FROM states WHERE name = "California");
 
-SELECT * FROM cities WHERE state_id = @california_id ORDER BY id ASC;
+SELECT id, name FROM cities WHERE state_id = @california_id ORDER BY id ASC;
