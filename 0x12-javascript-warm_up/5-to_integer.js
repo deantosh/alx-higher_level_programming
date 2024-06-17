6aabf5ecc8a681202d3fc9b3b044192788504959
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-06-17 11:47:50 -0700

    task 5: solution added

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /*Script prints My number: <first argument converted in integer>.
-If the first argument can be converted to an integer:*/
+If the first argument can be converted to an integer.
+*/
 if (process.argv[2]) {
   const num = parseInt(process.argv[2]);
   if (isNaN(num)) {
