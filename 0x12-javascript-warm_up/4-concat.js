1715568a53e03987d33597770e5828d5d9347d83
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-06-17 11:22:28 -0700

    task 4: solution added

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-// Script printsa sentence from  two arguments passed to it.
+// Script prints a sentence formed from two arguments passed to it.
 console.log(process.argv[2] + ' is ' + process.argv[3]);
