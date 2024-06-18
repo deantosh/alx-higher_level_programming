38881820b44960b4b3b7bd08229441a0fc91a8db
Deantosh Daiddoh <deantoshdaiddoh@gmail.com>
2024-06-18 12:36:54 -0700

    task 6: solution added

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+/*
+Write a class Square that defines a square and inherits from Square of 5-square.js:
+Requirements:
+ - You must use the class notation for defining your class and extends
+ - Create an instance method called charPrint(c) that prints the rectangle using the character c
+ - If c is undefined, use the character X
+*/
 const SquarePrev = require('./5-square');
 class Square extends SquarePrev {
   charPrint (c) {
