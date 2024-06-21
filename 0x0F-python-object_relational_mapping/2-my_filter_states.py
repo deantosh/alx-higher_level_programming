9d5ba43e6d9f8512fe13b7461e0692516946fc1a
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-06-21 00:48:57 -0700

    task 2: solution added

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,36 +1,58 @@
 #!/usr/bin/python3
 """
-This script takes in an argument and displays all
-values in the states table of hbtn_0e_0_usa where
-name matches the argument
+Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+Requirements:
+ - Your script should take 4 arguments: mysql username, mysql password,
+   database name and state name searched (no argument validation needed)
+ - You must use the module MySQLdb (import MySQLdb)
+ - Your script should connect to a MySQL server running on localhost at
+   port 3306
+ - You must use format to create the SQL query with the user input
+ - Results must be sorted in ascending order by states.id
+ - Results must be displayed as they are in the example below
+ - Your code should not be executed when imported
 """
-
-
-from sys import argv
+import sys
 import MySQLdb
+
 
 if __name__ == '__main__':
     """
-    Access to the db and get the states.
+    Connects to the database, displays all values in the states table
+    where name matches argument.
     """
-    # Create database connection
+
+    # retrieve state name
+    state_name = sys.argv[4]
+
+    # connect to the database
     conn = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        password=argv[2],
-        database=argv[3],
-        port=3306
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
-    # cursor obect to connect to database
+
+    # create cursor object - execute queries
     cursor = conn.cursor()
-    # Set the variabe in python
-    state_name = argv[4]
-    # Execute query
-    query = "SELECT * FROM states WHERE BINARY name LIKE '{}%'".format(
-        state_name)
+
+    # execute sql query
+    query = f"""
+             SELECT *
+             FROM states
+             WHERE BINARY states.name
+             LIKE '{state_name}%'
+             ORDER BY states.id ASC
+             """
     cursor.execute(query)
-    # Fetch results
+
+    # fetch results
     results = cursor.fetchall()
-    # Print results
     for row in results:
         print(row)
+
+    # close connections
+    cursor.close()
+    conn.close()
