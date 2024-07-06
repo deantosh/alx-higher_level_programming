d9fb547619f9f758ee2ae64b5eab54ae54070b19
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-07-06 14:00:12 -0700

    task 2: solution added

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-Script takes in a URL and an email, sends a POST request to the passed\
-URL with the email as a parameter, and displays the body of the\
+Script takes in a URL and an email, sends a POST request to the passed
+URL with thee email as a parameter, and displays the body of the
 response (decoded in utf-8).
+Requirements:
+  - The email must be sent in the email variable
+  - You are not allowed to import packages other than urllib and sys
+  - You must use the with statement
 """
-
 import sys
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
@@ -12,13 +15,12 @@ from urllib.parse import urlencode
 if __name__ == '__main__':
     # get args
     url, email = sys.argv[1:]
-
     # create data as dict object
-    value = {}
-    value["email"] = email
+    new_dict = {}
+    new_dict["email"] = email
 
-    data = urlencode(value).encode('ascii')  # data to bytes
-    req = Request(url, data)
+    data = urlencode(new_dict).encode('ascii')  # data to bytes
+    req = Request(url, data=data, method='POST')
     with urlopen(req) as response:
         body = response.read().decode('utf-8')
         print(body)
