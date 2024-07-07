fb56e856df125f70556e930733c563bbb543652b
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-07-07 12:56:58 -0700

    Task 6: solution added

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL and an email address, sends a POST request\
-to the passed URL with the email as a parameter, and finally displays\
+Script takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter, and finally displays
 the body of the response.
+Requirements:
+  - The email must be sent in the variable email
+  - You must use the packages requests and sys
+  - You are not allowed to import packages other than requests and sys
 """
-
 import sys
 import requests
 
