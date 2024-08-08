6c849ff0fe45aa262409b4f082403c370269bdeb
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-08-08 16:26:35 +0300

    task 8: solution added

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,3 +1,7 @@
-/* global $ */
 $(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $.each(data.results, function (index, movie) {
+      $('#list_movies').append('<li>' + movie.title + '</li>');
+    });
+  });
 });
