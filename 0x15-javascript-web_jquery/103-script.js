4b6ffd6362d49dd507a0cd9094bc501399d414d5
deantosh daiddoh <deantoshdaiddoh@gmail.com>
2024-08-08 19:07:22 +0300

    task 13: solution added

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,3 +1,7 @@
-/* global $ */
 $(document).ready(function () {
+  const langCode = $('#language_code').val();
+  const url = 'https://www.fourtonfish.com/hellosalut/hello/?lang=' + langCode;
+  $.get(url, function (data) {
+    $('#hello').text(data.hello);
+  });
 });
